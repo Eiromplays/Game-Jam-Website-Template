bb2c5515b36d673b4e56b5533da7e2a1d3da0232
Eirik Sjøløkken <e.sjoeloe@gmail.com>
2021-05-26 08:38:18 +0200

    Created the project.

---
+++ GameJam/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GameJam.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
